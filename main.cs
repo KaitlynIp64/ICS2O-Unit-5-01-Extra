04aebdf225510d25fb088f6d3fd5e33d6f8b9896
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-26 16:15:37 +0000

    added code

--- main.cs
+++ main.cs
@@ -1,7 +1,29 @@
+// Created by: Kaitlyn Ip
+// Created on: Apr 2022
+//
+// This program accepts user input
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Random random = new Random();
+        int returnValue = random.Next(1, 6);
+        int Guess = 0;
+        Console.WriteLine("Guess a number between 1-6.");
+        if (Guess != returnValue)
+        {
+            Guess = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Try again! The answer was " + returnValue);
+            Console.ReadLine();
+        }
+        if (Guess == returnValue)
+        {
+            Guess = Convert.ToInt32(Console.Read());
+            Console.WriteLine("Congratulations! The answer was " + returnValue);
+            Console.ReadLine();
+        }
+    }
 }
